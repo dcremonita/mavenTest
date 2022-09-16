afdff934a5e39f6f22dea23ef7fc70a39ddf0cdd
Dazi Cremonita <dcremonita@itcs-olympian.com>
2022-09-16 15:31:58 +0800

    working Maven project

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    static App numberGuessingGame = new App();
+    static int answer = 5;
+    @Test
+    void compareUserInputToAnswer() {
+
+
+        assertEquals("Your answer is too small.", numberGuessingGame.checkAnswer(answer, 3));
+        assertEquals("Your answer is too big.", numberGuessingGame.checkAnswer(answer, 7));
+        assertEquals("Your answer is correct!", numberGuessingGame.checkAnswer(answer, 5));
+
+        System.out.println("Congratulations, all your comparisons work!");
+    }
+
+    @Test
+    void checkCheckTries(){
+        assertEquals("You have 2 more tries.", numberGuessingGame.checkTries(2, answer));
+        assertEquals("You have 1 more try.", numberGuessingGame.checkTries(1, answer));
+        assertEquals("You have 0 tries left.\nThe correct answer is " + answer, numberGuessingGame.checkTries(0, answer));
+        //assertEquals("The correct answer is " + answer, numberGuessingGame.checkTries(0, answer));
+
+        System.out.println("Congratulations, your checkTries() method works!");
+    }
+
+
+
+
+}
